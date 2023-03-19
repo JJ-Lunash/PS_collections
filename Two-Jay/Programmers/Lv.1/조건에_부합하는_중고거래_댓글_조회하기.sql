031156e22c566305afd3b4493e683389168d5af1
Two-Jay <djeeee1272@gmail.com>
2023-03-20 00:54:53 +0900

    조건에_부합하는_중고거래_댓글_조회하기 : 스키마 업데이트

--- Two-Jay/Programmers/Lv.1/조건에_부합하는_중고거래_댓글_조회하기.sql
+++ Two-Jay/Programmers/Lv.1/조건에_부합하는_중고거래_댓글_조회하기.sql
@@ -2,7 +2,29 @@
 -- Level : 1
 -- Category : 프로그래머스
 -- URL : https://school.programmers.co.kr/learn/courses/30/lessons/164673
--- ==========================================================
+-- =========================================================================
+/*
+테이블 구조
+
+Table USED_GOODS_BOARD {
+  BOARD_ID  varchar(5)
+  WRITER_ID varchar(50)
+  TITLE     varchar(100)
+  CONTENTS  varchar(1000)
+  PRICE     NUMBER
+  CREATED_DATE  DATE
+  STATUS    varchar(10)
+  VIEWS     NUMBER
+}
+
+Table USED_GOODS_REPLY {
+  REPLY_ID  varchar(10)
+  BOARD_ID  varchar(5) [ref:> USED_GOODS_BOARD.BOARD_ID ]
+  WRITER_ID varchar(50)
+  CONTENTS  varchar(1000) [null]
+  CREATED_DATE DATE
+}
+*/
 
 SELECT BOARD.TITLE AS TITLE,
     BOARD.BOARD_ID AS BOARD_ID,
