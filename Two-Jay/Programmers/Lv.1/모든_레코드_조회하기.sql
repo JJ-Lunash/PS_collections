e037e57376802294296c1b24dde1ad7b82870352
Two-Jay <djeeee1272@gmail.com>
2023-03-20 00:14:56 +0900

    update : 모든 레코드 조회하기

---
+++ Two-Jay/Programmers/Lv.1/모든_레코드_조회하기.sql
@@ -0,0 +1,8 @@
+-- Title : 모든 레코드 조회하기
+-- Level : 1
+-- Category : 프로그래머스
+-- URL : https://school.programmers.co.kr/learn/courses/30/lessons/59034
+-- ==========================================================
+
+SELECT * FROM ANIMAL_INS AS animals
+ORDER BY animals.ANIMAL_ID ASC
