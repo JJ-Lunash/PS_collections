bfd660e2a900865c116e1c69a70adabe5ed72f4d
Two-Jay <djeeee1272@gmail.com>
2023-03-20 02:03:32 +0900

    강원도에_위치한_생산공장_목록_출력하기 : SUBSTR 설명 추가

--- Two-Jay/Programmers/Lv.1/강원도에_위치한_생산공장_목록_출력하기.sql
+++ Two-Jay/Programmers/Lv.1/강원도에_위치한_생산공장_목록_출력하기.sql
@@ -20,3 +20,18 @@ SELECT factories.FACTORY_ID,
 FROM FOOD_FACTORY AS factories
 WHERE factories.ADDRESS LIKE '%강원도%'
 ORDER BY factories.FACTORY_ID ASC;
+
+/*
+SUBSTR() 활용
+
+LIKE 가 아니라 SUBSTR()를 활용해서 풀어보면 다음과 같다.
+LIKE는 패턴 매칭을 기반으로한 문자열 검색이고
+SUBSTR()은 문자열의 일부를 추출하는 함수이다.
+
+SELECT factories.FACTORY_ID,
+    factories.FACTORY_NAME,
+    factories.ADDRESS
+FROM FOOD_FACTORY AS factories
+WHERE SUBSTR(factories.ADDRESS, 1, 3) = '강원도'
+ORDER BY factories.FACTORY_ID ASC;
+*/
